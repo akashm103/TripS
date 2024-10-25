c5c2370e10dda21029694c0afa730aa76328b444
akashm026 <76767089+akashm026@users.noreply.github.com>
2024-10-25 21:44:09 +0530

    application properties changes
    able to store in postgres

--- models/src/main/java/com/project/trips/itinerary/Item.java
+++ models/src/main/java/com/project/trips/itinerary/Item.java
@@ -1,5 +1,8 @@
 package com.project.trips.itinerary;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table
 public class Item {
+    @Id
     private String id;
     private String name;
     private String type;
--- models/src/main/java/com/project/trips/itinerary/Itinerary.java
+++ models/src/main/java/com/project/trips/itinerary/Itinerary.java
@@ -1,18 +1,24 @@
 package com.project.trips.itinerary;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table
 public class Itinerary {
+    @Id
     private String id;
     private String name;
     private String timestamp;
     private String description;
-    private LinkedList<String> items;
+    private ArrayList<String> items;
 }
